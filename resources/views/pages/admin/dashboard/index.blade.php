0dcabd655973233a85ff1a4b674428a99bbac20d
RivaldiIdris777 <flyingriders905@gmail.com>
2024-02-16 00:28:24 -0500

    Perbaikan dashboard

--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -14,7 +14,7 @@
         <div class="card bg-danger text-white mb-4">
             <div class="card-body">Data Peserta ( {{ count($peserta )}} )</div>
             <div class="card-footer d-flex align-items-center justify-content-between">
-                <a class="small text-white stretched-link" href="{{ route('peserta') }}">View Details</a>
+                <a class="small text-white stretched-link" href="{{ route('peserta.index') }}">View Details</a>
                 <div class="small text-white"><i class="fas fa-angle-right"></i></div>
             </div>
         </div>
